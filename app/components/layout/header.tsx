228dfb1ced135c4c45be2454b689585d09827cd1
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-06-03 12:03:58 -0400

    basic header created

---
+++ app/components/layout/header.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header aria-label="Page Header" className="bg-white dark:bg-black">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="flex flex-row items-center justify-between">
+          <div className="block gap-x-2">
+            <svg
+              width="28px"
+              height="28px"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#000000"
+            >
+              <path
+                d="M7 16v-4.639c0-.51.1-.999.285-1.453M17 16v-3.185m-7.778-5.08A5.506 5.506 0 0112 7c2.28 0 4.203 1.33 4.805 3.15M10 17v-2.177M14 17v-5.147C14 10.83 13.105 10 12 10s-2 .83-2 1.853v.794"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              anon
+            </h1>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="group flex shrink-0 items-center rounded-lg transition"
+            >
+              <span className="sr-only">Menu</span>
+              <img
+                alt="Man"
+                src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                className="h-10 w-10 rounded-full object-cover"
+              />
+
+              <p className="ms-2 hidden text-left text-xs sm:block">
+                <strong className="block font-medium">Eric Frusciante</strong>
+
+                <span className="text-gray-500"> eric@frusciante.com </span>
+              </p>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ms-4 hidden h-5 w-5 text-gray-500 transition group-hover:text-gray-700 sm:block"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
