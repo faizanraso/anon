8f0451190ff799fc0630404f85ff271f9f3ade88
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-06-04 20:17:54 -0400

    refactored

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -1,63 +1,8 @@
 import React from "react";
 import Dropdown from "../icons/Dropdown";
+import { programs, schools } from "@/app/utils/constants";
 
 export default function Sidebar() {
-  const schools = [
-    "Carleton University",
-    "Centennial College",
-    "Concordia University",
-    "Conestoga College",
-    "Dalhousie University",
-    "Durham College",
-    "Fanshawe College",
-    "George Brown College",
-    "Humber College",
-    "McGill University",
-    "McMaster University",
-    "Mohawk College",
-    "Queen's University",
-    "Ryerson University",
-    "Seneca College",
-    "Sheridan College",
-    "Simon Fraser University",
-    "University of Alberta",
-    "University of British Columbia",
-    "University of Calgary",
-    "University of Guelph",
-    "University of Manitoba",
-    "University of Montreal",
-    "University of Ottawa",
-    "University of Saskatchewan",
-    "University of Toronto",
-    "University of Victoria",
-    "University of Waterloo",
-    "Western University",
-    "Wilfrid Laurier University",
-    "York University",
-  ];
-
-  const programs = [
-    "Arts and Humanities",
-    "Business and Management",
-    "Engineering",
-    "Health and Medicine",
-    "Social Sciences",
-    "Education",
-    "Computer Science",
-    "Fine Arts and Design",
-    "Law and Legal Studies",
-    "Social Work",
-    "Architecture",
-    "Agriculture and Environmental Sciences",
-    "Communications and Media",
-    "Language and Linguistics",
-    "Hospitality and Tourism",
-    "Psychology",
-    "Mathematics and Statistics",
-    "Physical Education and Sports",
-    "Other",
-  ];
-
   return (
     <nav
       aria-label="Side Nav"
