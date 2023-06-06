0ed44c24a2d7333630884d346110f5604385fbfa
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-06-06 11:43:53 -0400

    sidebar dark mode

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -5,12 +5,12 @@ import Dropdown from "../icons/Dropdown";
 export default function () {
   return (
     <>
-      <div className="fixed inset-0 top-0 hidden w-60 overflow-y-auto bg-white sm:block">
+      <div className="fixed inset-0 top-0 hidden w-60 overflow-y-auto bg-white dark:bg-black sm:block">
         <div className="h-16">.</div>
         <nav aria-label="Side Nav" className="flex flex-col space-y-1">
           <a
             href=""
-            className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+            className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-semibold text-gray-700 underline dark:bg-black dark:text-gray-200"
           >
             Home
           </a>
@@ -18,8 +18,11 @@ export default function () {
             open
             className="group [&_summary::-webkit-details-marker]:hidden"
           >
-            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-              <span className="text-sm font-medium"> Programs </span>
+            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-black dark:hover:text-gray-200">
+              <span className="text-sm font-semibold underline">
+                {" "}
+                Programs{" "}
+              </span>
               <Dropdown />
             </summary>
 
@@ -31,7 +34,7 @@ export default function () {
                 <a
                   key={index}
                   href=""
-                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700  dark:text-gray-400 dark:hover:bg-black dark:hover:text-gray-200"
                 >
                   {program}
                 </a>
@@ -39,8 +42,8 @@ export default function () {
             </nav>
           </details>
           <details className="group [&_summary::-webkit-details-marker]:hidden">
-            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-              <span className="text-sm font-medium"> Schools </span>
+            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700  dark:text-gray-400 dark:hover:bg-black dark:hover:text-gray-200">
+              <span className="text-sm font-semibold underline"> Schools </span>
               <Dropdown />
             </summary>
             <nav
@@ -51,7 +54,7 @@ export default function () {
                 <a
                   key={index}
                   href=""
-                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700  dark:text-gray-400 dark:hover:bg-black dark:hover:text-gray-200"
                 >
                   {school}
                 </a>
