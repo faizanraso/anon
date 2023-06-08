ce4067b24af621b5d34ee75ac0fe244abe9b2fe1
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-06-08 10:39:29 -0400

    small changes

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
                 <svg
                   width="35px"
                   height="35px"
-                  stroke-width="2"
+                  strokeWidth="2"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                   color="#000000"
