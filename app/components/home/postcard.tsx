f6570cde6fa3a5d67dc69e52c75b78551680b977
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-06-28 17:26:37 -0400

    posts from database render

--- app/components/home/postcard.tsx
+++ app/components/home/postcard.tsx
@@ -9,7 +9,7 @@ export default function PostCard(props: {
 }) {
   return (
     <div className="px-5 md:px-1">
-      <div className="w-full rounded-md border border-black p-5 dark:border-gray-400">
+      <div className="relative h-52 max-h-52 w-full rounded-md border border-black p-5 dark:border-gray-400">
         <div className="flex flex-row justify-end gap-x-2 pb-1 text-right">
           <p className="rounded-full bg-sky-200 px-1.5 py-1 text-[10px] text-black">
             {props.school}
@@ -24,8 +24,8 @@ export default function PostCard(props: {
         <div className="pt-1.5">
           <p className="text-sm">{props.content}</p>
         </div>
-        <div className="pt-4">
-          <p className="text-xs text-gray-500 dark:text-gray-400">
+        <div className="absolute bottom-3 pt-4">
+          <p className="text-xs text-gray-500 dark:text-gray-400 ">
             {props.author}
           </p>
         </div>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,19 +1,109 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Modal from "../components/modal/Modal";
-import Posts from "../components/home/posts";
+import PostCard from "../components/home/postcard";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function retrievePosts() {
+      const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      };
+
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/getPosts",
+          requestOptions
+        );
+
+        if (!response.ok) {
+          return;
+        }
+        const responsePosts = await response.json();
+        setPosts(responsePosts);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    retrievePosts();
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <>
       <Modal />
       <main className="w-full p-3 sm:ml-60">
         <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <Posts />
+          <PostCard
+            key={"index"}
+            title={"post.title"}
+            content={"post.content"}
+            school={"post.school"}
+            program={"post.program"}
+            author={"test"}
+          />
+          <PostCard
+            key={"index"}
+            title={"post.title"}
+            content={"post.content"}
+            school={"post.school"}
+            program={"post.program"}
+            author={"test"}
+          />{" "}
+          <PostCard
+            key={"index"}
+            title={"post.title"}
+            content={"post.content"}
+            school={"post.school"}
+            program={"post.program"}
+            author={"test"}
+          />{" "}
+          <PostCard
+            key={"index"}
+            title={"post.title"}
+            content={"post.content"}
+            school={"post.school"}
+            program={"post.program"}
+            author={"test"}
+          />{" "}
+          <PostCard
+            key={"index"}
+            title={"post.title"}
+            content={"post.content"}
+            school={"post.school"}
+            program={"post.program"}
+            author={"test"}
+          />{" "}
+          <PostCard
+            key={"index"}
+            title={"post.title"}
+            content={"post.content"}
+            school={"post.school"}
+            program={"post.program"}
+            author={"test"}
+          />
+          {posts.map((post, index) => (
+            <PostCard
+              key={index}
+              title={post.title}
+              content={post.content}
+              school={post.school}
+              program={post.program}
+              author={"test"}
+            />
+          ))}
         </div>
       </main>
     </>
