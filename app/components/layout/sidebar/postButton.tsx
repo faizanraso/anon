979523342316338c596aef76686857f67af004e3
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-07-04 22:09:31 -0400

    made the sidebar server side and some of its components client side

---
+++ app/components/layout/sidebar/postButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default async function PostButton(props: { sessionStatus: any }) {
+  return (
+    <>
+      {props.sessionStatus ? (
+        <div className="flex flex-col items-center justify-center pb-3 pt-5">
+          <button className="inline-block rounded border border-blue-500 bg-blue-500 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-blue-500 focus:outline-none focus:ring active:text-blue-400">
+            Write a Post
+          </button>
+          <div className="w-10/12 border-0 border-b border-gray-200 pt-5 dark:border-gray-500"></div>
+        </div>
+      ) : null}
+    </>
+  );
+}
--- app/components/layout/sidebar/sidebar.tsx
+++ app/components/layout/sidebar/sidebar.tsx
@@ -1,63 +1,25 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
+import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 import { programs, schools } from "@/app/utils/constants";
 import Dropdown from "../../icons/Dropdown";
+import PostButton from "./PostButton";
+import MobileMenu from "./MobileMenu";
 
-export default function Sidebar() {
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  const { data: session, status } = useSession();
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      setLoggedIn(true);
-    }
-  }, [status]);
-
-  function mobileMenu() {
-    document.querySelector("#sidemenu")!.classList.toggle("hidden");
-    document.querySelector("#sidemenu")!.classList.toggle("z-20");
-  }
+export default async function Sidebar() {
+  const session = await getServerSession(authOptions);
 
   return (
     <>
-      <button
-        onClick={() => mobileMenu()}
-        className="fixed bottom-5 left-5 z-40 flex h-10 w-10 items-center justify-center rounded-full bg-gray-100 text-gray-900 hover:bg-gray-300 dark:bg-gray-900 dark:text-yellow-400 dark:hover:bg-gray-700 sm:hidden"
-      >
-        <svg
-          width="24px"
-          height="24px"
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="stroke-black transition-colors duration-150 dark:stroke-gray-100 "
-        >
-          <path
-            d="M3 5h18M3 12h18M3 19h18"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
-      </button>
+      <MobileMenu />
       <div
         id="sidemenu"
         className="fixed inset-0 top-0 hidden w-60 overflow-y-auto bg-gray-50 dark:bg-black sm:block"
       >
         <div className="h-16">.</div>
 
-        {loggedIn ? (
-          <div className="flex flex-col items-center justify-center pb-3 pt-5">
-            <button className="inline-block rounded border border-blue-500 bg-blue-500 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-blue-500 focus:outline-none focus:ring active:text-blue-400">
-              Write a Post
-            </button>
-            <div className="w-10/12 border-0 border-b border-gray-200 pt-5 dark:border-gray-500"></div>
-          </div>
-        ) : null}
+        <PostButton sessionStatus={session} />
 
         <nav aria-label="Side Nav" className="flex flex-col space-y-1">
           <a
