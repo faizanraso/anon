cbb083c62402e4f899687d44329f1da1d88c2fdb
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-07-16 16:52:51 -0400

    complete

--- app/components/posts/PostContent.tsx
+++ app/components/posts/PostContent.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import Comment from "./Comment";
+import React from "react";
+import Comments from "./Comments";
 import CommentBox from "./CommentBox";
 import useSWR from "swr";
 
 export default function PostContent(props: { postId: string }) {
-  const [postData, setPostData] = useState<any>();
-
   const fetcher = (url: RequestInfo | URL) =>
     fetch(url).then((res) => res.json());
 
@@ -17,11 +15,7 @@ export default function PostContent(props: { postId: string }) {
     fetcher
   );
 
-  useEffect(() => {
-    setPostData(data);
-  }, [data]);
-
-  if (!postData) return null;
+  if (!data) return null;
 
   if (isLoading) return null;
 
@@ -32,40 +26,38 @@ export default function PostContent(props: { postId: string }) {
       <section className="py-4">
         <div className="border border-gray-200 px-10 py-5 dark:border-neutral-700">
           <div>
-            <h1 className="text-3xl font-semibold">{postData.title}</h1>
+            <h1 className="text-3xl font-semibold">{data.title}</h1>
           </div>
           <div className="flex flex-row gap-2 py-5 text-gray-500 dark:text-gray-400">
             <p>
               <span className="text-sm transition duration-100 hover:text-gray-600 hover:underline dark:hover:text-gray-300">
-                <Link href="/">{postData.school}</Link>
+                <Link href="/">{data.school}</Link>
               </span>{" "}
               -{" "}
               <span className="text-sm transition duration-100 hover:text-gray-600 hover:underline dark:hover:text-gray-300">
-                <Link href={"/"}>{postData.program}</Link>
+                <Link href={"/"}>{data.program}</Link>
               </span>{" "}
               -{" "}
               <span className="text-sm transition duration-100 hover:text-gray-600 hover:underline dark:hover:text-gray-300">
-                <Link href={"/"}>{postData.username}</Link>
+                <Link href={"/"}>{data.username}</Link>
               </span>
             </p>
           </div>
           <div>
-            <p className="text-sm">{postData.content}</p>
+            <p className="text-sm">{data.content}</p>
           </div>
         </div>
       </section>
       <section className="flex w-full border border-gray-200 px-10 py-4 dark:border-neutral-700">
-        <CommentBox postId={props.postId} />
+        <CommentBox postId={data.post_id} />
       </section>
       <section className="mt-4 border border-gray-200 px-10  dark:border-neutral-700">
         <div className="py-4">
           <p className="text-xs text-gray-700 dark:text-gray-300">
-            {postData.comments.length} comment(s)
+            {data.comments.length} comment(s)
           </p>
         </div>
-        {postData.comments.map((comment: any, index: any) => (
-          <Comment comment={comment} />
-        ))}
+        <Comments postId={data.post_id} />
       </section>
     </main>
   );
