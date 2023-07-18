516da0d05ab9a2108fba24bb4f01541c8831a7ba
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-07-17 21:57:52 -0400

    Adding conditional styles

--- app/components/layout/sidebar/sidebar.tsx
+++ app/components/layout/sidebar/sidebar.tsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 import { programFilters, schoolFilters } from "@/app/utils/constants";
 import Dropdown from "../../icons/Dropdown";
 import PostModal from "../../modals/PostModal";
 import MobileMenu from "./MobileMenu";
+import SchoolDropdown from "./SchoolDropdown";
 
-export default async function Sidebar() {
-  const session = await getServerSession(authOptions);
+export default function Sidebar() {
+  const pathname = usePathname();
+  const [filterType, setFilterType] = useState<string>("");
+  const [filterId, setFilterId] = useState<string>("");
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    const paths = pathname.split("/");
+    if (paths.length > 2) {
+      const filter = paths[2];
+      if (schoolFilters.filter((school) => school.name === filter)) {
+        setFilterType("school");
+        setFilterId(paths[2]);
+      } else if (programFilters.filter((program) => program.name === filter)) {
+        setFilterType("program");
+        setFilterId(paths[1]);
+      } else {
+        null;
+      }
+    }
+  }, []);
 
   return (
     <>
@@ -29,30 +51,15 @@ export default async function Sidebar() {
         <nav aria-label="Side Nav" className="flex flex-col space-y-1 pt-2">
           <a
             href="/home"
-            className="block rounded-lg px-4 py-2 text-sm font-semibold text-gray-700 dark:bg-black dark:text-gray-200"
+            className={`block rounded-lg px-4 py-2 text-sm font-medium ${
+              filterType === "none"
+                ? "font-semibold text-gray-700"
+                : "text-gray-500"
+            } dark:bg-black dark:text-gray-200`}
           >
             Home
           </a>
-          <details className="group [&_summary::-webkit-details-marker]:hidden">
-            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:text-gray-700  dark:text-gray-400 dark:hover:bg-black dark:hover:text-gray-200">
-              <span className="text-sm font-semibold"> Schools </span>
-              <Dropdown />
-            </summary>
-            <nav
-              aria-label="Schools Nav"
-              className="mt-2 flex flex-col space-y-1 px-4"
-            >
-              {schoolFilters.map((school, index) => (
-                <a
-                  key={index}
-                  href={"/home/" + school.filterId}
-                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-black dark:hover:text-gray-200"
-                >
-                  {school.name}
-                </a>
-              ))}
-            </nav>
-          </details>
+          <SchoolDropdown filterId={filterId} filterType={filterType} />
           <details
             open
             className="group [&_summary::-webkit-details-marker]:hidden"
