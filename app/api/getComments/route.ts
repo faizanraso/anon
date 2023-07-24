5ea91a64067758938f335b1f329d00f8ffe0a638
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-07-23 21:54:07 -0400

    Comment Pagination

--- app/api/getComments/route.ts
+++ app/api/getComments/route.ts
@@ -4,13 +4,16 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const postId = searchParams.get("postId");
+  const scroll = Number(searchParams.get("commentLevel"));
 
   try {
+    const count = await prisma.comment.count({ where: { post_id: postId! } });
     const comments = await prisma.comment.findMany({
+      take: scroll * 3,
       orderBy: { creation_time: "desc" },
       where: { post_id: postId! },
     });
-    return NextResponse.json(comments);
+    return NextResponse.json({ comments, count });
   } catch (e) {
     return e;
   }
