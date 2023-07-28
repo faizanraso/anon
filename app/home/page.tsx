8e81e94860a6f459d4bd17fbf47d77eeee1bc8ab
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-07-27 22:06:10 -0400

    Small bug fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -39,26 +39,27 @@ export default function Home() {
       <InfoModal />
       <Toaster position="top-right" reverseOrder={false} />
       <main className="w-full p-3 sm:ml-60">
-        <section className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 min-h-screen">
-          {posts?.map(
-            (post: {
-              post_id: string;
-              title: string;
-              content: string;
-              school: string;
-              program: string;
-              username: string;
-            }) => (
-              <PostCard
-                key={post.post_id}
-                title={post.title}
-                content={post.content}
-                school={post.school}
-                program={post.program}
-                username={post.username}
-                postId={post.post_id}
-              />
-            )
+        <section className="min-h-screen">
+          {posts && posts.length > 0 ? (
+            <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {posts?.map((post) => (
+                <PostCard
+                  key={post.post_id}
+                  title={post.title}
+                  content={post.content}
+                  school={post.school}
+                  program={post.program}
+                  username={post.username}
+                  postId={post.post_id}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="flex items-center justify-center justify-items-center py-3">
+              <p className="text-md font-medium">
+                Looks like there are no posts in this category yet.
+              </p>
+            </div>
           )}
         </section>
         <section className="flex w-full items-center justify-center gap-x-2 p-5 ">
