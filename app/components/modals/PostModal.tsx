2f4c97da322ad77b5d0dbf0b5e743909fd94bae1
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-07-29 18:28:58 -0400

    Modal z-index correction

--- app/components/modals/PostModal.tsx
+++ app/components/modals/PostModal.tsx
@@ -58,7 +58,7 @@ export default function PostModal(props: { sessionStatus: any }) {
           </Dialog.Trigger>
           <Dialog.Portal>
             <Dialog.Overlay className="fixed inset-0" />
-            <Dialog.Content className="fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[600px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none dark:border dark:border-neutral-700 dark:bg-black">
+            <Dialog.Content className="fixed left-[50%] top-[50%] z-50 max-h-[85vh] w-[90vw] max-w-[600px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none dark:border dark:border-neutral-700 dark:bg-black">
               <Dialog.Title className="my-5 text-[17px] font-medium">
                 Write a Post 💭
               </Dialog.Title>
